1356855618bf7c74695ce7f5c0ca0426ca44d220
Jazelle Cruz <jazellecruz22@gmail.com>
2023-01-25 13:33:19 +0800

    connect db, define routes

--- server.js
+++ server.js
@@ -1,5 +1,23 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
+const cors = require("cors");
+const { connectDb } = require("./src/db/db");
+const library = require("./src/routes/library");
+
+app.use(cors({
+  origin: "http://localhost:3000",
+  method: ["GET", "POST", "DELETE", "PATCH", "OPTIONS"]
+}));
+
+// connect to database
+connectDb();
+
+app.use("/library", library);
+
+app.get("/", (req, res) => {
+  res.send("Booked main route.");
+});
 
 app.listen(8000, (req, res) => {
   console.log("Howdy from port 8000!");
