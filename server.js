4155a1a7d6d4fd7de6e56068f595290002fc29ba
Jazelle Cruz <jazellecruz22@gmail.com>
2023-01-28 18:14:42 +0800

    define categories route

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const app = express();
 const cors = require("cors");
 const { connectDb }= require("./src/db/db");
 const library = require("./src/routes/library");
+const categories = require("./src/routes/categories");
 
 app.use(cors({
   origin: "http://localhost:3000",
@@ -16,7 +17,11 @@ app.use(express.json());
 connectDb();
 
 //routes 
+// for the main homepage of user
 app.use("/library", library);
+
+//the categories route
+app.use("/library/category/", categories);
 
 app.get("/", (req, res) => {
   res.send("Booked main route.");
