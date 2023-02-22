bd913cc3158cf458fabcb547a22939d22dcee00a
Jazelle Cruz <jazellecruz22@gmail.com>
2023-02-22 15:43:43 +0800

    add route to handle  queries

--- src/routes/books.js
+++ src/routes/books.js
@@ -1,18 +1,37 @@
 const router = require("express").Router();
-const { getBooks, 
+const { getBooks,
+        filterBooks,
+        getBooksByCategory, 
         addBook,
         deleteBook, 
         modifyBook } = require("../controllers/books");
 
 // route for getting books
+router.get("/", async(req, res, next) => {
+  let response;
+  if(req.query) {
+    next();
+  } else {
+  response = await getBooks();
+  res.send(response)
+  }
+});
+
+//route to filter books
 router.get("/", async(req, res) => {
-  let response = await getBooks();
-  res.send(response);
+  let response = await filterBooks(req.query)
+  res.send(response)
 });
 
 // get a book by its id
-router.get("/id", async(req, res) => {
+router.get("/:id", async(req, res) => {
 
+});
+
+// filter books by category
+router.get("/", async(req, res) => {
+  let response = await getBooksByCategory(req.query["category_id"]);
+  res.send(response);
 });
 
 // add a book
