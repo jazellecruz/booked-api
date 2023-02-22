bedbed85a5634fe4be1b61b2a6a29b5767faba83
Jazelle Cruz <jazellecruz22@gmail.com>
2023-02-22 15:42:27 +0800

    remove getBooksByCategories function

--- src/controllers/categories.js
+++ src/controllers/categories.js
@@ -6,39 +6,6 @@ const getCategories = async() => {
   try{
     let results = await connection.promise().query("SELECT * FROM categories")
     response = results[0]
-    console.log(response)
-  } catch(err) {
-    throw err;
-  }
-  return response;
-}
-
-const getBooksByCategory = async(id) => {
-  let response;
-
-  try{
-    let results = await connection.promise().query(
-      `(SELECT books.book_id, title, author, description, categories.category, status.status, reviews.rating, reviews.comment, dateAdded 
-       FROM books
-       INNER JOIN reviews 
-       ON books.review_id = reviews.review_id
-       INNER JOIN status 
-       ON books.status_id = status.status_id
-       INNER JOIN categories 
-       ON books.category_id = categories.category_id
-       WHERE books.category_id = ${id});
-       (SELECT category FROM categories WHERE category_id = ${id});`)
-      
-    // the result sent is contained in an array that is inside in an another array
-    // to access it directly, is shifted and pop from its parent array
-    let books = results[0].shift();
-    let category = results[0].pop();
-
-    response = {
-      books: [...books],
-      // just spread the object since it has the same key name
-      ...category[0]
-    }
   } catch(err) {
     throw err;
   }
@@ -58,4 +25,4 @@ const addCategory = async(category) => {
   return response;
 } 
 
-module.exports = { getCategories, getBooksByCategory, addCategory };
+module.exports = { getCategories, addCategory };
