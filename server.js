cfab33714fbe74ae5406814a776e4768b70641c9
Jazelle Cruz <jazellecruz22@gmail.com>
2023-02-27 22:24:24 +0800

    add auth route

--- server.js
+++ server.js
@@ -2,14 +2,16 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const { connectDb }= require("./src/db/db");
+const { connectDb, connection }= require("./src/db/db");
+const bcrypt = require("bcrypt");
+const auth = require("./src/routes/auth.js")
 const books = require("./src/routes/books");
 const categories = require("./src/routes/categories");
 const reviews = require("./src/routes/reviews");
 
 app.use(cors({
   origin: "http://localhost:3000",
-  method: ["GET", "POST", "DELETE", "PATCH", "OPTIONS"]
+  method: "GET,POST,DELETE,PATCH,OPTIONS",
 }));
 
 app.use(express.json());
@@ -31,8 +33,32 @@ app.get("/", (req, res) => {
   res.send("Booked main route.");
 });
 
+app.use("/auth", auth);
+
 app.post("/test", (req, res) => {
-  console.log(req.body)
+  if (req.body) {
+    console.log(req.body)
+    return res.status(200).send({
+      message: 'Successfully saved!'
+   });
+  } else {
+    return res.status(400).send({
+      message: 'This is an error!'
+   });
+  }
+})
+
+app.delete("/test/:book_id", (req, res) => {
+  if(req.params.book_id) {
+    console.log(req.params.book_id)
+    return res.status(200).send({
+      message: 'Successfully deleted!'
+    });
+  } else {
+    return res.status(400).send({
+      message: 'No item deleted.'
+   })
+  }
 })
 
 app.listen(8000, () => {
