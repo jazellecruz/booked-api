bb919eb52f95196382786ad32e08e212a73d4276
Jazelle Cruz <jazellecruz22@gmail.com>
2023-02-28 20:55:33 +0800

    add auth logic

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,19 +1,33 @@
 const router = require("express").Router();
 const { connection } = require("../db/db");
 const bcrypt = require("bcrypt");
-const saltRounds = 10;
-const samplePassword = "mypassword";
+const { generateAccessToken, generateRefreshToken } = require("../utils/utils")
 
 router.post("/login", async(req, res) => {
-  const {user, password} = req.body
+  const {username, email, password} = req.body;
 
-  try {
-    let result = await connection.promise().query(
-      `SELECT password FROM users WHERE username = ${user}`)
+  try{
+    let results = await connection.promise().query(
+      `SELECT password FROM users WHERE username = "${username}" AND email = "${email}";`
+    )
+
+    if(results[0].length === 0){
+      res.status(404).send("No user with such credentials found.")
+    } else {
+      let match = await bcrypt.compare(password, results[0][0].password)
+      if(match) {
+        let accessToken = await generateAccessToken({username: username, email: email})
+        let refreshToken = await generateRefreshToken({username: username, email: email})
+        res.status(200).send({accessToken: accessToken, refreshToken: refreshToken})
+      } else {
+        res.status(401).send("Access denied.")
+      }
+    }
+
   } catch(err) {
-
+    console.log(err)
   }
-  res.status(200).send("Received credentials!")
+
 });
 
 module.exports = router
