27840ed0d9d8acb9d3aa5a4437a4ee8adc7a1e15
Jazelle Cruz <jazellecruz22@gmail.com>
2023-03-29 18:53:27 +0800

    remove test routes

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const auth = require("./src/routes/auth.js")
 const books = require("./src/routes/books");
 const categories = require("./src/routes/categories");
 const reviews = require("./src/routes/reviews");
+const status = require("./src/routes/status");
 const { isUserAuthenticated } = require("./src/middleware/auth")
 
 app.use(cors({
@@ -20,7 +21,12 @@ app.use(express.json());
 connectDb();
 
 //routes 
-// for the main homepage of user
+
+app.get("/", (req, res) => {
+  res.send("Booked main route.");
+});
+
+// for getting books
 app.use("/library/books", isUserAuthenticated, books);
 
 //the categories route
@@ -29,33 +35,11 @@ app.use("/library/categories",isUserAuthenticated, categories);
 // the reviews route
 app.use("/library/reviews",isUserAuthenticated, reviews);
 
-app.get("/", (req, res) => {
-  res.send("Booked main route.");
-});
+// status route
+app.use("/library/status",isUserAuthenticated, status);
 
+// for authentication of user
 app.use("/auth", auth);
-
-app.post("/test", (req, res) => {
-  if (req.body) {
-    console.log(req.body)
-    res.status(200).send('Successfully saved!');
-  } else {
-    res.status(500).send('Internal Server Error');
-  }
-})
-
-app.delete("/test/:book_id", (req, res) => {
-  if(req.params.book_id) {
-    console.log(req.params.book_id)
-    return res.status(200).send({
-      message: 'Successfully deleted!'
-    });
-  } else {
-    return res.status(400).send({
-      message: 'No item deleted.'
-   })
-  }
-})
 
 app.listen(8000, () => {
   console.log("Howdy from port 8000!");
