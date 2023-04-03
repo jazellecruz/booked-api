d45d5339c522e305285dcce3f475deab788d880e
Jazelle Cruz <jazellecruz22@gmail.com>
2023-04-03 22:19:48 +0800

    remove refresh token from response

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const { connection } = require("../db/db");
-const { generateAccessToken, generateRefreshToken } = require("../utils/utils")
+const { generateAccessToken } = require("../utils/utils")
 
 router.post("/login", async(req, res) => {
   const {username, password} = req.body;
@@ -10,7 +10,7 @@ router.post("/login", async(req, res) => {
   try{
     let results = await connection.promise().query(
       `SELECT password FROM users WHERE username = "${username}";`
-    )
+    );
 
     if(results[0].length === 0){
       res.status(404).send("No user with such credentials found.")
@@ -18,25 +18,23 @@ router.post("/login", async(req, res) => {
       let match = await bcrypt.compare(password, results[0][0].password)
       if(match) {
         let accessToken = generateAccessToken({username: username})
-        let refreshToken = generateRefreshToken({username: username})
         res.status(200).send({
           username: username,
-          accessToken: accessToken,
-          refreshToken: refreshToken})
+          accessToken: accessToken
+        });
       } else {
         res.status(401).send("Access denied.")
       }
     }
 
   } catch(err) {
-    console.log(err)
+    res.sendStatus(500);
   }
 
 });
 
 router.post("/verify", (req, res) => {
   let token = req.headers["x-access-token"]
-
   if(!token) {
     res.status(401).send("No token sent")
   } else {
@@ -44,10 +42,8 @@ router.post("/verify", (req, res) => {
       if(response) {
         res.status(200).send("Token Verified");
       } else if (err.name === "JsonWebTokenError") {
-        console.log(err);
-        res.status(500).send("Error in server")
+        res.status(500).send("Internal Server Error")
       } else {
-        console.log(err)
         res.status(401).send("Token Denied")
       }
     });
