7ea76de0f18e20a3000b5d1c11bc3e9a11865bdc
Jazelle Cruz <jazellecruz22@gmail.com>
2023-04-06 18:26:52 +0800

    add insert query for review  to addbook

--- src/controllers/books.js
+++ src/controllers/books.js
@@ -5,11 +5,11 @@ const getBooks = async(res) => {
   try{
     let results = await connection.promise().query(
       `START TRANSACTION;
-       SELECT books.book_id, title, author, description, img, categories.category, status.status, reviews.rating, reviews.comment, dateAdded FROM books
+       SELECT books.book_id, title, author, description, img, categories.category, status.status, reviews.rating, reviews.review, dateAdded FROM books
        left JOIN categories ON books.category_id = categories.category_id
        left JOIN reviews ON books.review_id = reviews.review_id
        left JOIN status ON books.status_id = status.status_id
-       ORDER BY reviews.rating DESC;
+       ORDER BY dateAdded DESC;
        SELECT COUNT(*) FROM books WHERE status_id = 1;
        COMMIT;`
        );
@@ -56,7 +56,7 @@ const filterBooks = async(query, res) => {
               categories.category, 
               status.status, 
               reviews.rating, 
-              reviews.comment, 
+              reviews.review, 
               dateAdded
       FROM books
       left JOIN categories ON books.category_id = categories.category_id
@@ -82,23 +82,38 @@ const filterBooks = async(query, res) => {
 
 }
 
-const addBook = async(book, res) => {
-  let { title, author, description, category_id, status_id, review_id, img } = book
+const addBook = async(entry, res) => {
+  let { title, author, description, category_id, status_id, img, rating, review } = entry
 
   try{
     let results = await connection.promise().query(
-      `INSERT INTO books (title, author, description, category_id, status_id, review_id, img, dateAdded)
+      `START TRANSACTION;
+       INSERT INTO books (title, author, description, category_id, status_id, img, dateAdded)
        VALUES ("${title}", 
               ${author ? `"${author}"` : null }, 
               ${description ? `"${description}"` : null }, 
               ${category_id}, 
               ${status_id}, 
-              ${review_id ? review_id : null}, 
               ${img ? `"${img}"` : null}, 
-              curdate());`);
+              curdate());
+       SET @last_book_id = LAST_INSERT_ID();
+       INSERT INTO reviews (reviews.book_id, rating, review)
+       VALUES (@last_book_id,
+               ${rating ? rating : null},
+               ${review ? `"${review}"` : null});
+       SET @last_review_id = LAST_INSERT_ID(); 
+       UPDATE books 
+       SET books.review_id = @last_review_id 
+       WHERE book_id = @last_book_id;
+       SELECT books.book_id, title, author, description, img, categories.category, status.status, reviews.rating, reviews.review, dateAdded FROM books
+       left JOIN categories ON books.category_id = categories.category_id
+       left JOIN reviews ON books.review_id = reviews.review_id
+       left JOIN status ON books.status_id = status.status_id
+       WHERE books.book_id = @last_book_id;
+       COMMIT;`);
     
-    if (results[0].affectedRows) {
-      res.status(200).send("Success");
+    if (results[0][1].affectedRows && results[0][3].affectedRows && results[0][5].affectedRows) {
+      res.status(200).send(results[0][6][0]);
     } else {
       res.status(202).send("Request acknowledged but not processed.");
     }
@@ -114,10 +129,19 @@ const addBook = async(book, res) => {
 const deleteBook = async(id, res) => {
   try{
     let results = await connection.promise().query(
-      `DELETE FROM books WHERE book_id="${id}";`)
+      `START TRANSACTION;
+       SET FOREIGN_KEY_CHECKS=0;
+       SET @review_id = (SELECT review_id FROM books WHERE book_id = "${id}");
+       DELETE FROM reviews WHERE review_id = @review_id;
+       DELETE FROM books WHERE books.book_id = ${id};
+       SET FOREIGN_KEY_CHECKS=1;
+       COMMIT;`)
     
+       //maybe try a query that will delete a parent row then delete its child row from a child table 
+
       // you can use !== 0 if there will be multiple rows affected
-    if (results[0].affectedRows) {
+      console.log(results[0])
+    if (results[0][3].affectedRows && results[0][4].affectedRows) {
       res.status(200).send("Item deleted successfully");
     } else {
       res.status(404).send("No item deleted. 0 resources matched.");
