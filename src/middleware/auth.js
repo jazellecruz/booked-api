b8ad5b3f13787346551d3ab9faf71d06ec648c80
Jazelle Cruz <jazellecruz22@gmail.com>
2023-04-06 15:03:12 +0800

    refactor

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -8,20 +8,19 @@ const isUserAuthenticated = async(req, res, next) => {
       let decode = await jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
       next();
     } catch(err) {
-      if (err.name === "TokenExpiredError") {
-        res.status(401).send("Token Expired");
-      } else if (err.name === "JsonWebTokenError") {
-        if(err.message === "secret or public key must be provided") {
-          res.status(500).send("Internal Server Error")
+      if(err.name === "JsonWebTokenError") {
+        if(err.message === "secret or public key must be provided"){
+          res.status(500).send("Failed Verification. Error in Server.")
         } else {
-          res.status(401).send("Invalid Token")
+          res.status(401).send("Invalid Token");
         }
       } else {
-        res.status(401).send("Token Denied")
+        res.status(401).send("Token Denied");
       }
     }
+
   } else {
-    res.status(401).send("No Token Received")
+    res.status(401).send("No Token Received");
   }
 }
 
