68125d6cc7d5b04015c9c6225e8928905b751b82
Jazelle Cruz <jazellecruz22@gmail.com>
2023-04-07 16:30:25 +0800

    catch exceptions

--- src/db/db.js
+++ src/db/db.js
@@ -4,7 +4,12 @@ const connection = mysql.createConnection(process.env.DB_URL);
 
 const connectDb = () => {
   connection.connect(err => {
-    if(err) throw err;
+    if(err) {
+      process.on('uncaughtException', (err) => {
+        console.error('Uncaught Exception:', err);
+        process.exit(1);
+      });
+    };
     console.log("Database connected...");
   });
 };
