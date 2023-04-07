9856fd0ecb0f127b47e3b3125dd8b6bde5d7f5f4
Jazelle Cruz <jazellecruz22@gmail.com>
2023-04-07 15:44:51 +0800

    configure

--- src/db/db.js
+++ src/db/db.js
@@ -1,9 +1,9 @@
 const mysql = require("mysql2");
 
 const connection = mysql.createConnection({
-  host     : 'localhost',
+  host     : process.env.DB_HOST,
   port     : 3300,
-  user     : 'root',
+  user     : process.env.DB_USER,
   password : process.env.DB_PASSWORD,
   database : 'booked_db',
   multipleStatements: true
