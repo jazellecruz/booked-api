b546d8ca5b7a7836419511c8cd5699ec28af2c73
Jazelle Cruz <jazellecruz22@gmail.com>
2023-04-12 14:56:35 +0800

    configure cors

--- server.js
+++ server.js
@@ -12,9 +12,9 @@ const { isUserAuthenticated } = require("./src/middleware/auth")
 
 app.options('*', cors());
 app.use(cors({
-  origin: ["https://booked-five.vercel.app/*"],
+  origin: ["https://booked-five.vercel.app/"],
   method: "GET,POST,DELETE,PATCH,OPTIONS,HEAD",
-  allowedHeaders: "Origin,Content-Type,Authorization,application/json,X-Requested-With,Accept,X-Api-Key,x-access-token",
+  allowedHeaders: ["Content-Type","Authorization","application/json","X-Requested-With","Accept","X-Api-Key","x-access-token"],
   credentials: true
 }));
 
